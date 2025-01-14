ce32f8288eaa7afbf15bbaa6d968dcbcd7f6f2c5
Christian Juarez <christianalexis0918@gmail.com>
2025-01-14 23:35:05 +0000

    Subida de Proyecto

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["logaligroup/main/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"logaligroup/main/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
