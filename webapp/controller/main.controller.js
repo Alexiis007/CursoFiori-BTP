072f22e3b11abfc25dc6f3d8608ef5d4cf7d0e29
Christian Juarez <christianalexis0918@gmail.com>
2025-01-15 00:09:05 +0000

    Video 4.3 Curso

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -3,8 +3,21 @@ sap.ui.define([
 ], (Controller) => {
     "use strict";
 
+    function myCheck(){
+        var inputEmployee = this.byId("inputEmployee");
+        var valueEmployee = inputEmployee.getValue();
+
+        if (valueEmployee.length == 6){
+            inputEmployee.setDescription("Ok");
+        }
+        else{
+            inputEmployee.setDescription("Not Ok")
+        }
+    }
+
     return Controller.extend("logaligroup.main.controller.main", {
         onInit() {
-        }
+        },
+        OnValidate : myCheck
     });
 });
